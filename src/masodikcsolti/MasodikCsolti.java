2be3d83411ad75afdd94dfc6f7055a35536b541b
repcsi90 <stumph.robert.andras@diak.szamalk-szalezi.hu>
2024-10-22 17:37:58 +0200

    Példányosítás

--- src/masodikcsolti/MasodikCsolti.java
+++ src/masodikcsolti/MasodikCsolti.java
@@ -1,12 +1,15 @@
 
 package masodikcsolti;
 
+import Osztályok.Ember;
+
 
 public class MasodikCsolti {
 
     
     public static void main(String[] args) {
-        
+        Ember ember = new Ember("Pál");
+        ember.bemutatkozik();
     }
     
     
