26e06768f4e7d642b070a9e4604bfdba2d70e680
repcsi90 <stumph.robert.andras@diak.szamalk-szalezi.hu>
2024-10-22 18:10:14 +0200

    KODDUPLIKÁLÁSSAL ujkstr.

--- src/Osztályok/Ember.java
+++ src/Osztályok/Ember.java
@@ -4,12 +4,45 @@ package Osztályok;
 
 public class Ember {
     private String nev;
-
-    public Ember(String nev) {
+    private int SzulEv;
+    private int munkaEv;
+    public Ember(String nev, int kor, int munkaEv) {
         this.nev = nev;
+        this.SzulEv = SzulEv;
+        this.munkaEv = munkaEv;
+        
+        
     }
-    public void bemutatkozik(){
-        System.out.println("A nevem: "+ nev);
+
+    public Ember(String nev, int SzulEv) {
+        this.nev = nev;
+        this.SzulEv = SzulEv;
+        this.munkaEv = 0;
+    }
+
+    
+    
+    public String bemutatkozik(){
+        return ("A nevem: "+ nev 
+                + "A korom:" + getKor() + ", " + munkaEv + "ennyi ideje dolgozom" );
+    }
+
+    public int getMunkaEv() {
+        return munkaEv;
+    }
+    public String szuletesEv(){
+        return "A korom: " + (2024-SzulEv);
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getSzulEv() {
+        return SzulEv;
+    }
+    public int getKor(){
+        return 2024-SzulEv;
     }
     
     
--- src/masodikcsolti/MasodikCsolti.java
+++ src/masodikcsolti/MasodikCsolti.java
@@ -8,9 +8,13 @@ public class MasodikCsolti {
 
     
     public static void main(String[] args) {
-        Ember ember = new Ember("Pál");
-        ember.bemutatkozik();
+        Ember ember = new Ember("Pál", 2000, 10);
+        System.out.println(ember.bemutatkozik());
+        Ember ember2 = new Ember("Pál", 2000);
+        System.out.println(ember2.bemutatkozik());
     }
+    
+    
     
     
     
